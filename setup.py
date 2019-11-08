23ac35ba027beedfb82666129aee9d01df6c2722
Patrick A. Levell <patrick29501@gmx.com>
2019-11-08 11:41:14 -0500

    Remove LICENSE

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='tweepy-trc',
+	version='0.1',
+	packages=['tweepy_trc'],
+	url='https://github.com/palevell/tweepy-trc',
+	license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
+	author='Patrick A. Levell',
+	author_email='patrick.a.levell@gmail.com',
+	description='Tweepy wrapper for .trc / .twurlrc'
+)
